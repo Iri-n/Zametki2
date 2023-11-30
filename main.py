f16dda0a3293dba5b61babc5a3c1a99f7f57936d
Irina Nunko <konyaevaik@gmail.com>
2023-11-30 22:02:46 +0300

    1) Json и кнопки на форме

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import ttk
 import uuid
 from datetime import datetime
+import json
+
+global my_data_list
+my_data_list = []
 
 root = Tk()
 root.title('Заметки')
@@ -58,5 +62,74 @@ trv.column("#1", anchor="w", width=270, stretch=True)
 trv.column("#2", anchor="w", width=140, stretch=False)
 trv.column("#3", anchor="w", width=140, stretch=False)
 trv.column("#4", anchor="w", width=140, stretch=False)
+
+
+def load_json_from_file():
+    global my_data_list
+    with open("notes.json", "r") as file_handler:
+        my_data_list = json.load(file_handler)
+    file_handler.close
+    print('file has been read and closed')
+
+
+def save_json_to_file():
+    global my_data_list
+    with open("notes.json", "w") as file_handler:
+        json.dump(my_data_list, file_handler, indent=4)
+    file_handler.close
+    print('file has been written to and closed')
+
+
+def remove_all_data_from_trv():
+    for item in trv.get_children():
+        trv.delete(item)
+
+
+def load_trv_with_json():
+    global my_data_list
+
+    remove_all_data_from_trv()
+
+    rowIndex = 1
+
+    for key in my_data_list:
+        n_id_value = key["id"]
+        name = key["name"]
+        date = key["date"]
+        note = key["note"]
+
+        trv.insert('', index='end', iid=rowIndex, text="",
+                   values=(n_id_value, name, date, note))
+        rowIndex = rowIndex + 1
+
+
+
+
+
+ButtonFrame = LabelFrame(root, text='', bg="lightgray", font=('Consolas', 14))
+ButtonFrame.grid(row=5, column=0, columnspan=6)
+
+
+btnShow = Button(ButtonFrame, text="Print", padx=20, pady=10)
+btnShow.pack(side=LEFT)
+
+btnAdd = Button(ButtonFrame, text="Add", padx=20, pady=10)
+btnAdd.pack(side=LEFT)
+
+btnUpdate = Button(ButtonFrame, text="Update", padx=20, pady=10)
+btnUpdate.pack(side=LEFT)
+
+btnDelete = Button(ButtonFrame, text="Delete", padx=20, pady=10)
+btnDelete.pack(side=LEFT)
+
+btnClear = Button(ButtonFrame, text="Cancel", padx=18, pady=10)
+btnClear.pack(side=LEFT)
+
+btnExit = Button(ButtonFrame, text="Exit", padx=20, pady=10)
+btnExit.pack(side=LEFT)
+
+
+load_json_from_file()
+load_trv_with_json()
 
 root.mainloop()
